aaedb789ed5d17757914797883e88ad024ed5d65
Vladislav Burlutskiy <vladislavburlutskiy@MacBook-Pro.local>
2019-09-21 19:52:43 -0400

    Building the app using classes, added a handleBpmChange function component to drag bpm

--- src/Metronome.js
+++ src/Metronome.js
@@ -1,40 +1,45 @@
-import React, {useState} from 'react';
+import React, { Component } from 'react';
 import './Metronome.css';
 
-
-const Metronome = ({count, beatsPerMeasure}) => {
-
-    const[playing, bpm] = useState({
-       
-      playing: false,
-      count: 0,
-      bpm: 100,
-      beatsPerMeasure: 4
-    },[])
-
-    const handleBpmChange = (e) => {
-        e.preventDefault();
-        console.log(e)
-        useState = e.target.value;
+class Metronome extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          playing: false,
+          count: 0,
+          bpm: 100,
+          beatsPerMeasure: 4
+        };
     }
+    handleBpmChange = event => {
+            const bpm = event.target.value;
+            this.setState({ bpm });
+      }
+    
 
+  render() {
+
+    const { playing, bpm } = this.state;
 
     return (
-        <div className='metronome'>
-            <div className='bpm-slider'>
-                <div>{bpm} BPM</div>
-                <input 
-                    type='range'
-                    min='60' 
-                    max='240' 
-                    value={bpm}
-                    onChange={handleBpmChange} />
-            </div>
-            <button>{playing ? 'Stop' : 'Start'}</button>
-
+      <div className="metronome">
+        <div className="bpm-slider">
+          <div>{bpm} BPM</div>
+          <input
+            type="range"
+            min="60"
+            max="240"
+            value={bpm}
+            onChange={this.handleBpmChange} />
         </div>
-    )
+        <button>
+          {playing ? 'Stop' : 'Start'}
+        </button>
+      </div>
+    );
+  }
+ 
 }
-
 
 export default Metronome;
